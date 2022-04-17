a5dee7136f73d97809df236ae12a5fb1423247ff
Kevin <floreskevin51398@gmail.com>
2022-04-16 22:48:11 -0500

    started the about page

---
+++ src/Pages/About/About.js
@@ -0,0 +1,23 @@
+
+
+import Nav from './components/Nav';
+import style from './About.css';
+export {About};
+
+
+function About(){
+
+
+    return(
+        <body>
+        <Nav />
+        <div className="About">
+            
+
+        </div>
+        
+        </body>
+
+
+    );
+}
