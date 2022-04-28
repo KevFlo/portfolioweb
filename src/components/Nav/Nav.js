cdda8df10a680534e562f57a6019ed94160ea708
Kevin <floreskevin51398@gmail.com>
2022-04-27 20:12:02 -0500

    removing unused imports

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Nav.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 export default Nav;
 
 function Nav(){
