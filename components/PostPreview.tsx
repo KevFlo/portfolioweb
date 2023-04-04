1e3776b35e510232cd7c389bced453cb639f1688
Kevin Flores <floreskevin51398@gmail.com>
2023-04-04 02:24:29 -0500

    fixing eslint? -kev

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const PostPreview = (props: PostMetadata) => {
    return <div>
         <Link  href={`DevLog/posts/${props.slug}`}>
-            <h2  >{props.title}</h2>
+            <h2>{props.title}</h2>
         </Link>
         <p>{props.subtitle}</p>
         <p>{props.date}</p>
